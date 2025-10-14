cbbc122be2ff3533726f4b218d007cdfeadd5ec6
Alexander Franks <afranks@gmail.com>
2025-10-13 17:56:19 -0700

    homework 2

---
+++ homework/homework2/tests/q1a.R
@@ -0,0 +1,27 @@
+test = list(
+  name = "q1a",
+  cases = list(
+    ottr::TestCase$new(
+      hidden = FALSE,
+      name = NA,
+      points = 0.3333333333333333,
+      code = {
+        test_that("q1a2", {
+          tol = 1e-3
+          expect_gt(qbeta(0.025, alpha, beta), 0.07) & expect_lt(qbeta(0.025, alpha, beta), 0.13)
+        })
+      }
+    ),
+    ottr::TestCase$new(
+      hidden = FALSE,
+      name = NA,
+      points = 0.3333333333333333,
+      code = {
+        test_that("q1a3", {
+          tol = 1e-3
+          expect_gt(qbeta(0.975, alpha, beta), 0.23) & expect_lt(qbeta(0.975, alpha, beta), 0.27)
+        })
+      }
+    )
+  )
+)
---
+++ homework/homework2/tests/q1c.R
@@ -0,0 +1,5 @@
+test = list(
+  name = "q1c",
+  cases = list(
+  )
+)
